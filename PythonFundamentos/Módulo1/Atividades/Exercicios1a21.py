233ede39c8fd1b168130315df22a95d51eabd364
CONATUS-Technologies <lucaspmonteiro420@gmail.com>
2026-02-05 12:08:40 -0300

    commit

---
+++ PythonFundamentos/Módulo1/Atividades/Exercicios1a21.py
@@ -0,0 +1,7 @@
+'''21) Desenvolva um programa que leia números inteiros até que o usuário digite 0. 
+   Ao final, mostre:
+    - A soma de todos os números positivos
+    - A média dos números pares
+    - O maior e o menor número digitado
+    - Quantos números negativos foram digitados
+'''
