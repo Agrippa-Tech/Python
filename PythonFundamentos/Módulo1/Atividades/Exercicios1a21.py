2d11c29fe587d4b128fa276f2bc2b19f77a7945b
CONATUS-Technologies <lucaspmonteiro420@gmail.com>
2026-02-07 13:59:34 -0300

    commit

--- PythonFundamentos/Módulo1/Atividades/Exercicios1a21.py
+++ PythonFundamentos/Módulo1/Atividades/Exercicios1a21.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 '''21) Desenvolva um programa que leia números inteiros até que o usuário digite 0. 
    Ao final, mostre:
@@ -8,48 +9,71 @@ import os
     - Quantos números negativos foram digitados
 '''
 
-numeros = []
-pares = []
-numeros_negativos = []
+try:
+    numeros = []
+    pares = []
+    numeros_negativos = []
 
-while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("\rDigite um número inteiro ou pressione [0] para sair: ", end= "", flush =True)
-    numero = int(input())
-    
-    if numero != 0:
-        numeros.append(numero)
-        
-        somar = 0
-        for num in numeros:
-            if num > 0:
-                somar += num
-        
-        if numero % 2 == 0:
-            pares.append(numero)
-            somar_pares = 0
-            for num in pares:
-                somar_pares += num
-                media_pares = (somar_pares) / len(pares) 
-        else:
-            somar_pares = f"""Não há nenhum número par"""
-            media_pares = f"""Não há nenhum número par"""
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        tentativas_user = 5
+        while tentativas_user > 0:
+            try:
+                print("\rDigite um número inteiro ou pressione [0] para sair: ", end= "", flush =True)
+                numero = int(input())
+                break
+                
+            except ValueError:
+                tentativas_user -= 1
+                if tentativas_user > 0:
+                    time.sleep(1)
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    print("\nERRO: DIGITE APENAS NÚMEROS INTEIROS")
+                    continue
+                else:
+                    time.sleep(2)
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    erro = "ERRO: REINICIE O PROGRAMA"
+                    for i in range(1, 15):
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        print(f"\r{erro}", end = " ", flush = True)
+                        time.sleep(0.5)
+                break
             
-        if numero < 0:
-            numeros_negativos.append(numero)
-            len(numeros_negativos)
+        if numero != 0:
+            numeros.append(numero)
+            
+            somar = 0
+            for num in numeros:
+                if num > 0:
+                    somar += num
+            
+            if numero % 2 == 0:
+                pares.append(numero)
+                somar_pares = 0
+                for num in pares:
+                    somar_pares += num
+                    media_pares = (somar_pares) / len(pares) 
+            else:
+                somar_pares = f"""Não há nenhum número par"""
+                media_pares = f"""Não há nenhum número par"""
+                
+            if numero < 0:
+                numeros_negativos.append(numero)
+                len(numeros_negativos)
+            
+        else:
+            print(somar)
+            print(somar_pares)
+            print(media_pares)
+            print(max(numeros))
+            print(min(numeros))
+            print(len(numeros_negativos))
+            break
+            
+except NameError:
+    print()       
         
-    else:
-        print(somar)
-        print(somar_pares)
-        print(media_pares)
-        print(max(numeros))
-        print(min(numeros))
-        print(len(numeros_negativos))
-        break
-        
-    
-    
        
     
     
